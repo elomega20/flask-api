f489ce85e7b6f3112ed38368775ec66bdc55cb8a
elomega20 <104397369+elomega20@users.noreply.github.com>
2023-03-01 19:08:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import os
+from doc_plus_pertinent import doc_plus_pertinent
 
 app = Flask(__name__)
 
@@ -7,6 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+@app.route('/api/v1/articles/<string:sujet_rechercher>', methods=['GET'])
+def process_json(sujet_rechercher):
+    response = doc_plus_pertinent(sujet_rechercher)
+    return response
 
 
 if __name__ == '__main__':
